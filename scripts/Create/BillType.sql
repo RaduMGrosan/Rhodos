8621ce2a73ba86bd6ce0ff56403758f933e15907
radu <Pc@RADU>
2024-01-23 16:23:55 +0200

    add more scripts

---
+++ scripts/Create/BillType.sql
@@ -0,0 +1,27 @@
+USE [Rhodos]
+GO
+
+/****** Object:  Table [dbo].[BillType]    Script Date: 1/23/2024 4:22:51 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[BillType]') AND type in (N'U'))
+DROP TABLE [dbo].[BillType]
+GO
+
+/****** Object:  Table [dbo].[BillType]    Script Date: 1/23/2024 4:22:51 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[BillType](
+	[Id] [bigint] NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[Value] [nvarchar](max) NULL,
+ CONSTRAINT [PK_BillType] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
